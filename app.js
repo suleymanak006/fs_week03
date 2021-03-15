2c8c77c23ef31820bcf89fdd2b94de1b8a5142db
suleymanak006 <suleymanak0006@gmail.com>
2021-03-15 01:54:24 +0100

    nodejs de dosya okuma

---
+++ app.js
@@ -0,0 +1,8 @@
+import http from "http";//server olusturmak icin gerekli kutuphane
+import fs from "fs"; // dosya okumak icin gerekli kutuphane (dosyalar npm icinden gelecegi icin ./ uzantisina gerek yok)
+
+const metin = function(oku){
+
+    console.log("./OrnekDosya.txt".toString());
+}
+console.log(metin);
